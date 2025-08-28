dea2f6ea712cedf61df8f9a8b54edf2787cb6a5f
abakymuk <vladimirovelyan@gmail.com>
2025-08-28 21:04:15 +0300

    feat: hook formatting test

---
+++ packages/lib/src/tmp.ts
@@ -0,0 +1,12 @@
+// Тестовый файл с плохим форматированием
+const badFormatting = {
+  name: 'test',
+  value: 123,
+  items: [1, 2, 3, 4, 5],
+}
+
+function testFunction(param1: string, param2: number) {
+  return param1 + param2.toString()
+}
+
+export { badFormatting, testFunction }
