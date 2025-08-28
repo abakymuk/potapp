177db8f63e65aa1c14c49bd3cca9eecb6dfd1c48
abakymuk <vladimirovelyan@gmail.com>
2025-08-28 20:04:09 +0300

    fix: remove broken import to restore CI

--- apps/web/src/index.ts
+++ apps/web/src/index.ts
@@ -2,7 +2,4 @@ import { helloLib } from '@potlucky/lib'
 
 import { formatMessage } from '@/utils/helpers'
 
-// Intentionally broken import to test CI
-import { brokenImport } from 'non-existent-package'
-
 export const webBoot = () => `web-ready with ${helloLib()} and ${formatMessage('test')}`
